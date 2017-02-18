f71c380a5e5a1593d8182f5a475891f3619644c9
Julian Feild <jfeild1337@users.noreply.github.com>
2017-02-17 19:28:20 -0500

    Update download_resources.sh to list what Ansible variables correspond to downloaded filenames

--- download_resources.sh
+++ download_resources.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-RESOURCE_DIR=resources
-############# ELASTICSEARCH #####################
-ELASTICSEARCH_RPM=elasticsearch-5.2.1.rpm
-KIBANA_RPM=kibana-5.2.1-x86_64.rpm
-ELASTICCO_GPG_KEY=GPG-KEY-elasticsearch
-#ELASTICSEARCH_LOCAL_FILE_DIR=roles/elasticsearch/files
+# Downloads various RPMs and other resources from the Internet.
+# NOTE: if you change the version of any downloaded file, 
+# make sure to change the appropriate Ansible variable. The 
+# variable and the Ansible file in which it's defined will be noted
+# for each filename here in a comment
 
-# Download ElasticSearch RPM
+# ANSIBLE VARIABLE: local_resource_prefix ANSIBLE FILE: group_vars/all
+RESOURCE_DIR=resources
+
+#----------------------- ELASTICSEARCH -----------------------
+
+# ANSIBLE VARIABLE: elasticsearch_rpm ANSIBLE FILE: group_vars/elasticsearch
+ELASTICSEARCH_RPM=elasticsearch-5.2.1.rpm
+
+# ANSIBLE VARIABLE: kibana_rpm ANSIBLE FILE: group_vars/elasticsearch
+KIBANA_RPM=kibana-5.2.1-x86_64.rpm
+
+# ANSIBLE VARIABLE: elasticco_gpg_key ANSIBLE FILE: group_vars/all
+ELASTICCO_GPG_KEY=GPG-KEY-elasticsearch
+
+
 wget "https://artifacts.elastic.co/downloads/elasticsearch/$ELASTICSEARCH_RPM" --directory-prefix=$RESOURCE_DIR
-# Download Kibana RPM
 wget "https://artifacts.elastic.co/downloads/kibana/$KIBANA_RPM" --directory-prefix=$RESOURCE_DIR
-# Download ES GPG Key
 wget "https://artifacts.elastic.co/$ELASTICCO_GPG_KEY" --directory-prefix=$RESOURCE_DIR
 
-#############   RABBITMQ   #####################
+#----------------------- RABBITMQ -----------------------
+
+# ANSIBLE VARIABLE: rabbit_mq_gpg_key ANSIBLE FILE: group_vars/task_broker
 RABBITMQ_GPG_KEY=rabbitmq-release-signing-key.asc
+
 # folder in downloads section
 RABBITMQ_VERSION=v3.6.6
-RABBITMQ_RPM=rabbitmq-server-3.6.6-1.el7.noarch.rpm
-#RABBITMQ_LOCAL_FILE_DIR=roles/task_broker/files
 
-# Download RabbitMQ RPM
+# ANSIBLE VARIABLE: rabbit_mq_rpm ANSIBLE FILE: group_vars/task_broker
+RABBITMQ_RPM=rabbitmq-server-3.6.6-1.el7.noarch.rpm
+
 wget "http://www.rabbitmq.com/releases/rabbitmq-server/$RABBITMQ_VERSION/$RABBITMQ_RPM" --directory-prefix=$RESOURCE_DIR
 wget "https://www.rabbitmq.com/$RABBITMQ_GPG_KEY" --directory-prefix=$RESOURCE_DIR

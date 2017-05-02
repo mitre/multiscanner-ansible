2f9506543a3cf598f032d6e5f51cae9e02f1f755
Julian Feild <jfeild1337@users.noreply.github.com>
2017-05-02 11:30:31 -0400

    Update Gluster download and setup to normalize Gluster RPM versions

--- download_resources.sh
+++ download_resources.sh
@@ -41,13 +41,39 @@ wget "https://www.rabbitmq.com/$RABBITMQ_GPG_KEY" --directory-prefix=$RESOURCE_D
 
 #----------------------- GLUSTER ----------------------
 # ANSIBLE VARIABLE: gluster_version ANSIBLE FILE: group_vars/all
+ANSIBLE_VERSION_STR=NORMALIZED
+
 GLUSTER_VERSION=3.10.0
+GLUSTER_VERSION_MAJOR_MINOR=3.10
 mkdir $RESOURCE_DIR/gluster_server
 yumdownloader glusterfs-server-$GLUSTER_VERSION --resolve --destdir $RESOURCE_DIR/gluster_server
-tar -cvzf $RESOURCE_DIR/gluster_server.tgz -C $RESOURCE_DIR gluster_server
 
 mkdir $RESOURCE_DIR/gluster_client
 yumdownloader glusterfs-client --resolve --destdir $RESOURCE_DIR/gluster_client
+
+# Rename all the RPMS so that they work with the Ansible installer. Some RPMs will have
+# slightly different update version (i.e., 3.10.0 and 3.10.1) but this is not consistent
+# and Ansible yum can't handle wildcards 
+# SERVER:
+SERVER_RPMS=( glusterfs-api- glusterfs-cli-  \
+              glusterfs-client-xlators- \
+              glusterfs-fuse- glusterfs-libs- glusterfs-server- )
+for sr_ in ${SERVER_RPMS[@]}; do    
+    mv $RESOURCE_DIR/gluster_server/${sr_}*.rpm $RESOURCE_DIR/gluster_server/${sr_}$ANSIBLE_VERSION_STR.rpm      
+done
+# main gluster RPM is a special case
+mv $RESOURCE_DIR/gluster_server/glusterfs-$GLUSTER_VERSION*.rpm $RESOURCE_DIR/gluster_server/glusterfs-$ANSIBLE_VERSION_STR.rpm
+
+# CLIENT:
+CLIENT_RPMS=( glusterfs-api- glusterfs-client-xlators- \
+              glusterfs-fuse- glusterfs-libs- )
+for cr_ in ${CLIENT_RPMS[@]}; do    
+    mv $RESOURCE_DIR/gluster_client/${cr_}*.rpm $RESOURCE_DIR/gluster_client/${cr_}$ANSIBLE_VERSION_STR.rpm      
+done
+# main gluster RPM is a special case
+mv $RESOURCE_DIR/gluster_client/glusterfs-$GLUSTER_VERSION_MAJOR_MINOR*.rpm $RESOURCE_DIR/gluster_client/glusterfs-$ANSIBLE_VERSION_STR.rpm
+
+tar -cvzf $RESOURCE_DIR/gluster_server.tgz -C $RESOURCE_DIR gluster_server
 tar -cvzf $RESOURCE_DIR/gluster_client.tgz -C $RESOURCE_DIR gluster_client
 
 #---------------------- PYTHON 3----------------------
